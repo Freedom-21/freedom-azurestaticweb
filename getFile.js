4980d6a971c6358b3a32607432a7804c600d8c06
Freedom-21 <126236362+Freedom-21@users.noreply.github.com>
2024-06-10 22:47:40 +0200

    Update getFile.js

--- getFile.js
+++ getFile.js
@@ -1,1 +1,21 @@
+module.exports = async function (context, req) {
+    const fileName = req.query.filename;
+    if (!fileName) {
+        context.res = {
+            status: 400,
+            body: "Please provide a filename."
+        };
+        return;
+    }
+
+    const baseUrl = "https://publicfiles21.blob.core.windows.net/public-files/";
+    const fileUrl = `${baseUrl}${encodeURIComponent(fileName)}`;
+
+    context.res = {
+        status: 302,
+        headers: {
+            Location: fileUrl
+        }
+    };
+};
 
